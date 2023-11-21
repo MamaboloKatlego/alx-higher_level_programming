243179a4aafcc3f68dd10971e0cb5ca21b76477a
Mamabolo Katlego <rabotaikatlego@gmail.com>
2023-11-21 09:42:18 +0000

    task 8

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[], x=0):
-	printed = 0
-	try:
-		if x > 0:
-			for num in my_list[:x]:
-				print(str(num), end='')
-				printed = printed + 1;
-	except IndexError as e:
-		print("")
-		return (printed)
-	print("")
-	return (printed)
+def safe_function(fct, *args):
+    try:
+        res = fct(*args)
+        return res
+    except (ZeroDivisionError, IndexError) as error:
+        import sys
+        print("Exception: {}".format(error), file=sys.stderr)
+        return None

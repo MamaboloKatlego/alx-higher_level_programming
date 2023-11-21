b6571e8395ab75eccc4c63f7c5b164108069604c
Mamabolo Katlego <rabotaikatlego@gmail.com>
2023-11-21 07:55:50 +0000

    task 1

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    count = 0
-    while x > 0:
-        try:
-            print("{}".format(my_list[count]), end="")
-            count += 1
-            x -= 1
-        except (IndexError, ValueError, TypeError):
-            pass
-        except Exception:
-            print("Catch all")
-        finally:
-            print("")
-    return count
+        count = 0
+        for i in range(0, x):
+                try:
+                        print("{}".format(my_list[i]), end="")
+                        count = count + 1
+                except IndexError:
+                        break
+        print("")
+        return count

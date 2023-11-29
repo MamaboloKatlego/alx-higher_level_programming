1ec78dbdf475a6a954e1d250465eb12bb26c8afc
Mamabolo Katlego <rabotaikatlego@gmail.com>
2023-11-28 22:35:25 -0800

    task 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,30 +1,22 @@
 #!/usr/bin/python3
 """
-Addition Module
+add_integer:
+    Checks if parameters are int
+    Returns sum of parameters
 """
 
 
 def add_integer(a, b=98):
     """
-    Function to add two integers or floats
-
-    Args:
-        a (number): Float or Number
-        b (:obj:number:optional): Optional Float or Integer
-
-    Raises:
-        TypeError: If not float or integer
-
-    Returns:
-        float:int: Integer or float
+    Checks if int, otherwise return sum
     """
-
-    if (not isinstance(a, int) and not isinstance(a, float)):
-        raise TypeError("a must be an integer")
-    if (not isinstance(b, int) and not isinstance(b, float)):
-        raise TypeError("b must be an integer")
-    if isinstance(a, float):
+    if type(a) == float or type(b) == float:
         a = int(a)
-    if isinstance(b, float):
         b = int(b)
-    return a + b
+
+    if type(a) != int:
+        raise TypeError("a must be an integer")
+    elif type(b) != int:
+        raise TypeError("b must be an integer")
+    else:
+        return a + b

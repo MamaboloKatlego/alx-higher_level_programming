05080620adfa4c6bfad856143cd1e18821ad6fad
Mamabolo Katlego <rabotaikatlego@gmail.com>
2023-11-28 22:47:20 -0800

    5-text_indentation.py

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,33 +1,35 @@
 #!/usr/bin/python3
-"""
-Module to print after . ? and :
+""" text_indentation returns "text" in the specified format:
+2 newlines after each ['.', '?', ':']
 """
 
 
 def text_indentation(text):
+    """ prints "text" with 2 newlines after each of these char: ['.', '?', ':']
+    checks if "text" is a str
+    first loop removes spaces after each required chars
+    second loop adds 2 newlines after each required chars
     """
-    Write a function that prints a text with 2 new lines after each of these characters: ., ? and :
-
-    Args:
-        test (str): string
-
-    Raises:
-        TypeError: error
-    """
-
-    if not isinstance(text, str):
+    if type(text) != str:
         raise TypeError("text must be a string")
-    none_found = True
-    start = 0
-    for i in range(len(text)):
+    toCatAfter = ['.', '?', ':']
 
-        if (ord(text[i]) == ord(".") or ord(text[i]) == ord("?") or ord(text[i]) == ord(":")):
-            none_found = False
-            string_to_print = text[start:i + 1].strip()
-            print(f"{string_to_print}\n\n", end="")
-            if (i + 1) <= (len(text) - 1):
-                start = i + 1
-    if none_found:
-        print(text.strip(), end="")
-    elif start != (len(text) - 1):
-        print(text[start:len(text)].strip(), end="")
+    # Removes the space after special chars
+    idx = 0
+    for items in text:
+        if items in toCatAfter:
+            if text[idx + 1] == " ":
+                text = text[:idx + 1] + text[idx + 2:]
+        else:
+            idx += 1
+
+    # Cats '\n\n' after the special char with removed space
+    idx = 0
+    for items in text:
+        if items in toCatAfter:
+            text = text[:idx + 1] + '\n\n' + text[idx + 1:]
+            idx += 3
+        else:
+            idx += 1
+
+    print(text, end='')

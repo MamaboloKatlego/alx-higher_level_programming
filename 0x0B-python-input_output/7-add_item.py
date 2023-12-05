acfede1462c793290eab1a020aa51cbe4b59fa32
Mamabolo Katlego <rabotaikatlego@gmail.com>
2023-12-05 10:28:29 -0800

    task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,6 +1,24 @@
 #!/usr/bin/python3
 
-load_from_json = __import__("6-load_from_json_file").load_from_json
-save_to_json = __import__("5-save_tojson_file").save_to_json
+"""
+File: 7-add_item.py
+Description: The module deals with loading, adding, and savig.
+Author: Mamabolo Katlego <rabotaikatlego@gmail.com>
+Date Created: 05 Dec 2023
+"""
+from sys import argv as av
+from os.path import exists
+save = __import__('5-save_to_json_file').save_to_json_file
+load = __import__('6-load_from_json_file').load_from_json_file
 
-list = []
+name = "add_item.json"
+
+av.remove(av[0])
+pre_loaded = []
+
+if exists('add_item.json'):
+    pre_loaded = load(name)
+
+pre_loaded += av
+
+save(pre_loaded, name)

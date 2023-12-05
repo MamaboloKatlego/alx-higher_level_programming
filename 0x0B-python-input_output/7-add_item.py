fdb1d8adeca7e8fcd13d176f5e7e66192921423f
Mamabolo Katlego <rabotaikatlego@gmail.com>
2023-12-05 10:17:03 -0800

    task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,25 +1,18 @@
 #!/usr/bin/python3
-"""
-Module doc
-"""
-from sys import argv
-from os import path
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+# 7-add_item.py
+# MamaboloKatlego <rabotaikatlego@gmail.com>
+""" create a list from all arguments """
+import sys
 
 
-def add_items():
-    """
-    function docs
-    """
-    filename = "add_item.json"
-    if path.isfile(filename):
-        final_list = load_from_json_file(filename)
-    else:
-        final_list = []
-    for i in range(1, len(argv)):
-        final_list.append(argv[i])
-    save_to_json_file(final_list, filename)
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
 
-
-add_items()
+    try:
+        arguments = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        arguments = []
+    arguments.extend(sys.argv[1:])
+    save_to_json_file(arguments, "add_item.json")
